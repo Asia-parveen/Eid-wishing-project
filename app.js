99521b23d348a7b6ff5dc879ac387a75c2c3627e
Asia-parveen <beautyhealthytips@gmail.com>
2024-04-21 09:28:30 -0700

    add all files

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let array = ["KAMRAN TESSORI", "DANIYAL NAGORRI", "ZIA KHAN", "AMEEN ALAM", "HAMZAH SYED"];
+for (let i = 1; i < array.length; i++) {
+    console.log(`EID MUBARAK to sir ${array[i]} wish u a very happy eid \n from "Asia"`);
+}
+;
---
+++ app.ts
@@ -0,0 +1,5 @@
+let array = ["KAMRAN TESSORI","DANIYAL NAGORRI","ZIA KHAN","AMEEN ALAM","HAMZAH SYED"];
+for(let i = 1; i < array.length; i++){
+    console.log(`EID MUBARAK to sir ${array[i]} wish u a very happy eid \n from "Asia"`);
+    
+};
